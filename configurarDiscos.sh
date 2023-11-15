cf936164273493bcd04322a5d2f9f95ea5774101
Julian Malarino <50273308+jmalarino@users.noreply.github.com>
2023-11-15 16:30:59 -0300

    Update configurarDiscos.sh

--- configurarDiscos.sh
+++ configurarDiscos.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 
 mostrar_discos() {
-    echo "Listado de discos:"
+    echo -e "\e[1;32mListado de discos:\e[0m"
     fdisk -l
 }
 
 actualizar_discos() {
-    echo -e "\nActualizando información de discos..."
+    echo -e "\n\e[1;32mActualizando información de discos...\e[0m"
     partprobe
+    echo -e "\e[1;34m¡Información de discos actualizada!\e[0m"
 }
 
 buscar_espacio_no_asignado() {
-    echo -e "\nBuscando espacio no asignado..."
+    echo -e "\n\e[1;32mBuscando espacio no asignado...\e[0m"
     lvdisplay
 }
 
 listar_particiones_expansibles() {
-    echo -e "\nParticiones LVM que pueden expandirse:"
+    echo -e "\n\e[1;32mParticiones LVM que pueden expandirse:\e[0m"
     particiones_expansibles=($(lvdisplay | awk '/LV Path/ {path=$3} /LV Size/ {size=$3} /Allocatable/ {if ($2=="yes") print path, size}' | cut -d' ' -f1))
 
     if [ ${#particiones_expansibles[@]} -eq 0 ]; then
-        echo "No hay particiones LVM disponibles para expandirse."
+        echo -e "\e[1;34mNo hay particiones LVM disponibles para expandirse.\e[0m"
         return
     fi
 
@@ -36,24 +37,25 @@ expandir_particion() {
         if [ -n "$particion" ]; then
             lvextend -l +100%FREE /dev/mapper/$particion
             resize2fs /dev/mapper/$particion
-            echo "La partición LVM $particion se ha expandido al 100% del espacio disponible."
+            echo -e "\n\e[1;34mLa partición LVM $particion se ha expandido al 100% del espacio disponible.\e[0m"
             break
         else
-            echo "Opción no válida. Intente de nuevo."
+            echo -e "\e[1;31mOpción no válida. Intente de nuevo.\e[0m"
         fi
     done
 }
 
 while true; do
-    echo -e "\n--- Menú Principal ---"
-    echo "1. Mostrar discos"
-    echo "2. Actualizar información de discos"
-    echo "3. Buscar espacio no asignado"
-    echo "4. Listar particiones LVM que pueden expandirse"
-    echo "5. Expandir partición LVM"
-    echo "6. Salir"
+    echo -e "\n--- \e[1;33mMenú Principal\e[0m ---"
+    echo "1. \e[1;36mMostrar discos\e[0m"
+    echo "2. \e[1;36mActualizar información de discos\e[0m"
+    echo "3. \e[1;36mBuscar espacio no asignado\e[0m"
+    echo "4. \e[1;36mListar particiones LVM que pueden expandirse\e[0m"
+    echo "5. \e[1;36mExpandir partición LVM\e[0m"
+    echo "6. \e[1;36mRefrescar datos de particiones\e[0m"
+    echo "7. \e[1;31mSalir\e[0m"
 
-    read -p "Seleccione una opción (1-6): " opcion
+    read -p "Seleccione una opción (1-7): " opcion
 
     case $opcion in
         1) mostrar_discos ;;
@@ -61,7 +63,9 @@ while true; do
         3) buscar_espacio_no_asignado ;;
         4) listar_particiones_expansibles ;;
         5) expandir_particion ;;
-        6) echo "Saliendo del script. ¡Hasta luego!"; exit ;;
-        *) echo "Opción no válida. Intente de nuevo." ;;
+        6) continue ;;
+        7) echo -e "\e[1;31mSaliendo del script. ¡Hasta luego!\e[0m"; exit ;;
+        *) echo -e "\e[1;31mOpción no válida. Intente de nuevo.\e[0m" ;;
     esac
 done
+

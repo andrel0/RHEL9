b61f8fa5001e70d5f407594c6ac8c328dbfe3b94
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-23 19:16:45 -0300

    Update configurarDiscos.sh

--- configurarDiscos.sh
+++ configurarDiscos.sh
@@ -12,23 +12,18 @@ refrescar_y_detectar_discosnuevos() {
     echo "Listado de discos físicos:"
     lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
     echo -e "\nActualizando información de discos..."
-    
-    # Escanear y agregar discos físicos detectados localmente en VMware
-    echo "Escaneando y agregando discos físicos detectados localmente en VMware..."
-    rescan-scsi-bus.sh > /dev/null  # Se ejecuta sin mostrar la salida en pantalla
-    partprobe > /dev/null  # Se ejecuta sin mostrar la salida en pantalla
 
     # Obtener la lista de discos físicos nuevos sin particiones ni LVM
     discos_nuevos_disponibles=()
 
     while read -r disco tipo; do
-        # Asegúrate de que el disco no tenga una tabla de particiones conocida
-        if ! parted /dev/$disco print | grep -q "Partition Table: unknown"; then
+        # Asegúrate de que el disco no tenga particiones ni LVM
+        if [ "$tipo" == "disk" ] && [ -z "$(lsblk /dev/$disco -o NAME | tail -n +2)" ]; then
             espacio_disponible=$(lsblk -o SIZE -b -n /dev/$disco)
             echo "- $disco (Espacio Disponible: $espacio_disponible bytes)"
             discos_nuevos_disponibles+=($disco)
         fi
-    done < <(lsblk -o NAME,TYPE | awk '$2 == "disk" && system("lvdisplay " $1 " > /dev/null") == 1 && system("vgdisplay " $1 " > /dev/null") == 1 {print $1, $2}')
+    done < <(lsblk -o NAME,TYPE | tail -n +2)
 
     if [ ${#discos_nuevos_disponibles[@]} -eq 0 ]; then
         echo "No se han encontrado discos físicos nuevos con espacio disponible y sin particiones ni LVM asignados."
@@ -36,8 +31,13 @@ refrescar_y_detectar_discosnuevos() {
     fi
 
     echo "Discos físicos nuevos detectados con espacio disponible y sin particiones ni LVM asignados:"
-    # Ya no es necesario iterar sobre la variable discos_nuevos_disponibles, ya que la información se imprime en el bucle anterior
+    # la información se imprime en el bucle anterior
 }
+
+# Ejemplo de uso:
+refrescar_y_detectar_discosnuevos
+# echo "Discos nuevos disponibles: ${discos_nuevos_disponibles[@]}"
+
 
 listar_particiones_expandibles() {
     limpiar_pantalla

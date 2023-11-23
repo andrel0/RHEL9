d28a1ab28b962742aad5c80b227c1e60199264bd
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-23 18:36:27 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -10,7 +10,8 @@
     partprobe > /dev/null  # Se ejecuta sin mostrar la salida en pantalla
 
     # Obtener la lista de discos físicos nuevos sin particiones ni LVM
-discos_nuevos_disponibles=($(lsblk -o NAME,TYPE | awk '$2 == "disk" && system("vgdisplay " $1 " > /dev/null") == 1 && system("parted /dev/" $1 " print | grep -q \x27^$\x27") == 0 {print $1}'))
+   discos_nuevos_disponibles=($(lsblk -o NAME,TYPE | awk '$2 == "disk" && !("vgdisplay "$1 | getline) && !("parted /dev/"$1" print" | getline) {print $1}'))
+
 
     echo "Lista de discos obtenida: ${discos_nuevos_disponibles[@]}"
 
@@ -27,27 +28,3 @@ discos_nuevos_disponibles=($(lsblk -o NAME,TYPE | awk '$2 == "disk" && system("v
             echo "- $disco (Espacio Disponible: $espacio_disponible bytes)"
         fi
     done
-
-
-    Listado de discos físicos:
-NAME                            SIZE TYPE MOUNTPOINT
-sda                              30G disk
-├─sda1                          600M part /boot/efi
-├─sda2                            1G part /boot
-└─sda3                         28.4G part
-  ├─vg_rhel_rhel9-root           20G lvm  /
-  ├─vg_rhel_rhel9-swap            2G lvm  [SWAP]
-  └─vg_rhel_rhel9-home          6.4G lvm  /home
-sdb                              10G disk
-└─sdb1                           10G part
-  ├─vg_rhel_rhel9_dinamico-var    5G lvm  /var
-  └─vg_rhel_rhel9_dinamico-tmp    5G lvm  /tmp
-sdc                               1G disk
-sr0                             8.9G rom
-
-Actualizando información de discos...
-Escaneando y agregando discos físicos detectados localmente en VMware...
-Warning: Unable to open /dev/sr0 read-write (Read-only file system).  /dev/sr0 has been opened read-only.
-./debug.sh: line 27: unexpected EOF while looking for matching `"'
-./debug.sh: line 30: syntax error: unexpected end of file
-

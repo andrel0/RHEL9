9cbad389faae5ff44f308400b05df010b499e190
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-24 12:29:00 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -20,7 +20,7 @@ discos_nuevos_disponibles=()
 
 while read -r disco tipo; do
     # Asegúrate de que el disco no tenga particiones
-    if [ "$tipo" == "disk" ] && [ -z "$(fdisk -l /dev/$disco 2>/dev/null | grep -E '/dev/'$disco':[0-9]+')" ]; then
+    if [ "$tipo" == "disk" ] && [ -z "$(parted /dev/$disco print | grep -E 'Partition Table: unknown')" ] && [ -z "$(lvdisplay /dev/$disco 2>/dev/null)" ]; then
         espacio_disponible=$(lsblk -o SIZE -b -n /dev/$disco)
         echo "- $disco (Espacio Disponible: $espacio_disponible bytes)"
         discos_nuevos_disponibles+=($disco)
@@ -33,36 +33,3 @@ else
     echo "Discos físicos nuevos detectados con espacio disponible y sin particiones ni LVM asignados:"
     # Ya no es necesario iterar sobre la variable discos_nuevos_disponibles, ya que la información se imprime en el bucle anterior
 fi
-
-Listado de discos físicos:
-NAME                            SIZE TYPE MOUNTPOINT
-sda                              30G disk
-├─sda1                          600M part /boot/efi
-├─sda2                            1G part /boot
-└─sda3                         28.4G part
-  ├─vg_rhel_rhel9-root           20G lvm  /
-  ├─vg_rhel_rhel9-swap            2G lvm  [SWAP]
-  └─vg_rhel_rhel9-home          6.4G lvm  /home
-sdb                              10G disk
-└─sdb1                           10G part
-  ├─vg_rhel_rhel9_dinamico-var    5G lvm  /var
-  └─vg_rhel_rhel9_dinamico-tmp    5G lvm  /tmp
-sdc                               1G disk
-sr0                             8.9G rom
-
-Actualizando información de discos...
-Escaneando y agregando discos físicos detectados localmente en VMware...
-Warning: Unable to open /dev/sr0 read-write (Read-only file system).  /dev/sr0 has been opened read-only.
-- sda (Espacio Disponible: 32212254720
-  629145600
- 1073741824
-30507270144
-21474836480
- 2147483648
- 6878658560 bytes)
-- sdb (Espacio Disponible: 10737418240
-10735321088
- 5368709120
- 5364514816 bytes)
-- sdc (Espacio Disponible: 1073741824 bytes)
-./debug.sh: line 35: syntax error: unexpected end of file

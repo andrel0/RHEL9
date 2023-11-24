34a3a569cedffb47e9a261fae144c98226d2e9d5
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-24 12:34:25 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -13,14 +13,14 @@ echo -e "\nActualizando información de discos..."
 # Obtener la lista de discos físicos nuevos sin particiones
 discos_nuevos_disponibles=()
 
-while read -r disco tipo montaje; do
+for disco in $(lsblk -o NAME,TYPE | awk '$2 == "disk" {print $1}'); do
     # Asegúrate de que el disco no tenga particiones
-    if [ "$tipo" == "disk" ] && [ -z "$montaje" ] && [ -z "$(parted /dev/$disco print 2>/dev/null | grep -E 'Partition Table:')" ]; then
+    if [ -z "$(parted /dev/$disco print 2>/dev/null | grep -E 'Partition Table:')" ]; then
         espacio_disponible=$(lsblk -o SIZE -b -n /dev/$disco)
         echo "- $disco (Espacio Disponible: $espacio_disponible bytes)"
         discos_nuevos_disponibles+=($disco)
     fi
-done < <(lsblk -o NAME,TYPE,MOUNTPOINT | tail -n +2)
+done
 
 if [ ${#discos_nuevos_disponibles[@]} -eq 0 ]; then
     echo "No se han encontrado discos físicos nuevos sin particiones."

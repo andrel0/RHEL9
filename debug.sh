916c0d1ea332d7e7f0d538fca64234fc7a9f626b
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-28 18:13:34 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -4,7 +4,17 @@ function obtener_discos_nuevos() {
     local discos_nuevos_disponibles=()
 
     echo "Listado de discos físicos sin particiones reconocibles por LVM o con tabla de particiones desconocida:"
-    discos_nuevos_disponibles=($(lsblk -rno NAME,TYPE,MOUNTPOINT | awk '$2 == "disk" && $3 == "" {print $1}'))
+
+    for disco in /sys/class/block/sd*; do
+        # Verificar si el disco tiene particiones reconocibles
+        if [ ! -d "$disco"/[a-z]* ]; then
+            # Verificar si el disco tiene una tabla de particiones desconocida o es reconocible por LVM
+            if ! parted /dev/$(basename $disco) print 2>/dev/null | grep -qE '(Partition Table: unknown|lvm)'; then
+                echo "- $(basename $disco)"
+                discos_nuevos_disponibles+=("$(basename $disco)")
+            fi
+        fi
+    done
 
     if [ ${#discos_nuevos_disponibles[@]} -eq 0 ]; then
         echo "No se han encontrado discos físicos nuevos sin particiones reconocibles por LVM o con tabla de particiones desconocida."
@@ -16,9 +26,3 @@ function obtener_discos_nuevos() {
 
 # Ejecutar la función principal
 obtener_discos_nuevos
-
-
-./debug.sh
-Listado de discos físicos sin particiones reconocibles por LVM o con tabla de particiones desconocida:
-Discos físicos nuevos detectados sin particiones reconocibles por LVM o con tabla de particiones desconocida:
-Nombres de discos físicos nuevos: sda sdb sdc

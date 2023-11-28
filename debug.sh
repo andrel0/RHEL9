61716634e06334e0e929d086f892b08de72d2304
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-28 18:09:27 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Función para obtener discos nuevos sin particiones reconocibles
 function obtener_discos_nuevos() {
-    local discos_nuevos_globales=()
+    local discos_nuevos_disponibles=()
 
     echo "Listado de discos físicos sin particiones reconocibles por LVM o con tabla de particiones desconocida:"
     discos_nuevos_disponibles=($(lsblk -rno NAME,TYPE,MOUNTPOINT | awk '$2 == "disk" && $3 == "" {print $1}'))
@@ -12,30 +11,8 @@ function obtener_discos_nuevos() {
     else
         echo "Discos físicos nuevos detectados sin particiones reconocibles por LVM o con tabla de particiones desconocida:"
         echo "Nombres de discos físicos nuevos: ${discos_nuevos_disponibles[@]}"
-        discos_nuevos_globales=("${discos_nuevos_disponibles[@]}")
-    fi
-
-    echo "${discos_nuevos_globales[@]}"
-}
-
-# Función para mostrar información adicional sobre los discos
-function mostrar_informacion_adicional() {
-    local nombres_discos_nuevos=("$@")
-
-    if [ ${#nombres_discos_nuevos[@]} -gt 0 ]; then
-        echo -e "\nInformación adicional sobre los discos físicos nuevos:"
-        for disco in "${nombres_discos_nuevos[@]}"; do
-            echo -e "\n$disco:"
-            if parted /dev/$disco print 2>/dev/null | grep -qE '(Partition Table: unknown|lvm)'; then
-                parted /dev/$disco print
-            else
-                echo "El disco no tiene una tabla de particiones reconocible o es reconocible por LVM."
-            fi
-        done
     fi
 }
 
 # Ejecutar la función principal
-nombres_discos_nuevos=($(obtener_discos_nuevos))
-mostrar_informacion_adicional "${nombres_discos_nuevos[@]}"
-
+obtener_discos_nuevos

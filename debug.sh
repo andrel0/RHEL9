4a0949654638b6435ac9c79585e046e8f23d383f
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-28 18:00:01 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,45 +1,66 @@
 #!/bin/bash
 
-# Declaración de variables global
-discos_nuevos_globales=()
+# Función para limpiar la pantalla
+function limpiar_pantalla() {
+    clear
+}
 
-# Limpiar la pantalla
-clear
+# Función para obtener discos nuevos sin particiones reconocibles
+function obtener_discos_nuevos() {
+    local discos_nuevos_globales=()
 
-# Mostrar el listado de discos físicos
-echo "Listado de discos físicos:"
-lsblk -o NAME,SIZE,TYPE,MOUNTPOINT
+    limpiar_pantalla
 
-# Actualizar información de discos
-echo -e "\nActualizando información de discos..."
+    echo "Listado de discos físicos sin particiones reconocibles por LVM o con tabla de particiones desconocida:"
+    discos_nuevos_disponibles=($(lsblk -rno NAME,TYPE,MOUNTPOINT | awk '$2 == "disk" && $3 == "" {print $1}'))
 
-# Obtener la lista de discos físicos nuevos sin particiones
-discos_nuevos_disponibles=()
-
-for disco in $(lsblk -o NAME,TYPE | awk '$2 == "disk" {print $1}'); do
-    # Asegúrate de que el disco no tenga particiones
-    if [ -n "$(parted /dev/$disco print 2>/dev/null | grep 'Partition Table: unknown')" ]; then
-        espacio_disponible=$(lsblk -o SIZE -b -n /dev/$disco)
-        echo "- $disco (Espacio Disponible: $espacio_disponible bytes)"
-        discos_nuevos_disponibles+=($disco)
+    if [ ${#discos_nuevos_disponibles[@]} -eq 0 ]; then
+        echo "No se han encontrado discos físicos nuevos sin particiones reconocibles por LVM o con tabla de particiones desconocida."
+    else
+        echo "Discos físicos nuevos detectados sin particiones reconocibles por LVM o con tabla de particiones desconocida:"
+        echo "Nombres de discos físicos nuevos: ${discos_nuevos_disponibles[@]}"
+        discos_nuevos_globales=("${discos_nuevos_disponibles[@]}")
     fi
+
+    echo "${discos_nuevos_globales[@]}"
+}
+
+# Función para mostrar información adicional sobre los discos
+function mostrar_informacion_adicional() {
+    local nombres_discos_nuevos=("$@")
+
+    if [ ${#nombres_discos_nuevos[@]} -gt 0 ]; then
+        echo -e "\nInformación adicional sobre los discos físicos nuevos:"
+        for disco in "${nombres_discos_nuevos[@]}"; do
+            echo -e "\n$disco:"
+            if parted /dev/$disco print 2>/dev/null | grep -qE '(Partition Table: unknown|lvm)'; then
+                parted /dev/$disco print
+            else
+                echo "El disco no tiene una tabla de particiones reconocible o es reconocible por LVM."
+            fi
+        done
+    fi
+}
+
+# Menú de opciones
+while true; do
+    echo -e "\nMenú:"
+    echo "1. Obtener discos nuevos sin particiones reconocibles"
+    echo "2. Salir"
+
+    read -p "Seleccione una opción: " opcion
+
+    case $opcion in
+        1)
+            nombres_discos_nuevos=($(obtener_discos_nuevos))
+            mostrar_informacion_adicional "${nombres_discos_nuevos[@]}"
+            ;;
+        2)
+            echo "Saliendo del script."
+            exit 0
+            ;;
+        *)
+            echo "Opción no válida. Por favor, seleccione una opción válida."
+            ;;
+    esac
 done
-
-if [ ${#discos_nuevos_disponibles[@]} -eq 0 ]; then
-    echo "No se han encontrado discos físicos nuevos sin particiones."
-else
-    echo "Discos físicos nuevos detectados sin particiones:"
-    # Guardar todos los discos en el array global
-    discos_nuevos_globales=("${discos_nuevos_disponibles[@]}")
-    echo "Nombres de discos físicos nuevos: ${discos_nuevos_globales[@]}"
-fi
-
-# Mostrar información adicional sobre los discos detectados
-if [ ${#discos_nuevos_globales[@]} -gt 0 ]; then
-    echo -e "\nInformación adicional sobre los discos físicos nuevos:"
-    for disco in "${discos_nuevos_globales[@]}"; do
-        echo -e "\n$disco:"
-        parted  /dev/$disco print
-        # Puedes agregar más comandos para obtener información adicional según tus necesidades
-    done
-fi

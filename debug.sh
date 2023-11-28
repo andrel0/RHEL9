baf8b3f19ce0c733338a49d843260747028c862a
Juan Carresi <andrel0@users.noreply.github.com>
2023-11-28 18:18:20 -0300

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -5,14 +5,11 @@ function obtener_discos_nuevos() {
 
     echo "Listado de discos físicos sin particiones reconocibles por LVM o con tabla de particiones desconocida:"
 
-    for disco in /sys/class/block/sd*; do
-        # Verificar si el disco tiene particiones reconocibles
-        if [ ! -d "$disco"/"$(basename $disco)"[0-9]* ]; then
-            # Verificar si el disco tiene una tabla de particiones desconocida o es reconocible por LVM
-            if ! parted /dev/$(basename $disco) print 2>/dev/null | grep -qE '(Partition Table: unknown|lvm)'; then
-                echo "- $(basename $disco)"
-                discos_nuevos_disponibles+=("$(basename $disco)")
-            fi
+    for disco in $(lsblk -rno NAME,TYPE,MOUNTPOINT | awk '$2 == "disk" && $3 == "" {print $1}'); do
+        # Verificar si el disco tiene una tabla de particiones desconocida o es reconocible por LVM
+        if ! parted /dev/$disco print 2>/dev/null | grep -qE '(Partition Table: unknown|lvm)'; then
+            echo "- $disco"
+            discos_nuevos_disponibles+=("$disco")
         fi
     done
 
@@ -27,11 +24,3 @@ function obtener_discos_nuevos() {
 # Ejecutar la función principal
 obtener_discos_nuevos
 
-
-[root@rhel9 ~]# ./debug.sh
-Listado de discos físicos sin particiones reconocibles por LVM o con tabla de particiones desconocida:
-./debug.sh: line 10: [: too many arguments
-- sda1
-- sda2
-Discos físicos nuevos detectados sin particiones reconocibles por LVM o con tabla de particiones desconocida:
-Nombres de discos físicos nuevos: sda1 sda2
